0fb270efbf283e8a958e7e7504a7bcd6c1d7e025
zouyaowen <1227701903@qq.com>
2018-04-23 23:28:34 +0800

    处理器映射器案例

---
+++ springmvc-handlerMapping/src/main/java/com/kedacom/controller/MyController.java
@@ -0,0 +1,23 @@
+package com.kedacom.controller;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class MyController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView modelAndView = new ModelAndView();
+		System.out.println("执行MyController的handleRequest方法");
+		modelAndView.setViewName("/index.html");
+		return modelAndView;
+	}
+
+	
+
+   
+}
